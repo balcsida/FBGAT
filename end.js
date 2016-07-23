3804996184abff8872f29c191bf56eb7c8282d72
Dávid Balatoni <balcsida@gmail.com>
2016-07-23 17:01:13 +0200

    Comment fixes

--- end.js
+++ end.js
@@ -1,15 +1,16 @@
 $(function() {
 
-	//Recently Blocked Members ///////////////////////////////////////
+	// Recently Blocked Members 
 	$(document).on("mouseover", "html body:has(.groupJumpLayout:has(.groupsEditDescriptionArea ._4t38))", function() {
 		var grupid = $("input[name='profile_id']").attr("value");
 		$(this).find("._4-u2._3-96._4-u8:first:not(.oki)").addClass('oki ').prepend('<div class="sag-text son-engellenen"><a  href="/groups/' + grupid + '/blocked/?order=date"><b>Recently Blocked Members</b></a></div>');
 		$(".UFIRow").find(".engel:not(:eq(0))").remove();
 		$(this).find('.UFIRow:not(.hazir):not(.oki)').addClass("hazir").find('a.uiLinkSubtle').parent().prepend("<span class='yrm engel'><a href='#' rel='dialog-post' role='menuitem' tabindex='-1'><span><span> Block </span></span></a> - </span>");
 
-	}); //Recently Blocked Members ///////////////////////////////////////	
+	});
 
-	//YORUMDAN ENGELLEME ///////////////////////////////////////
+
+	// Add "Block" link to comments
 	$(document).on("mouseover", "html body:has(.groupJumpLayout:has(.groupsEditDescriptionArea ._4t38)) .hazir.UFIRow:not(.oki)", function() {
 		var grupid = $("input[name='profile_id']").attr("value");
 		var kimo = $(this).find('a.UFIImageBlockImage').html();
@@ -22,9 +23,8 @@ $(function() {
 		$(this).addClass("oki");
 		$(this).find(".yrm.engel").remove();
 
-	}); //YORUMDAN ENGELLEME ///////////////////////////////////////					   
 
-	//BEGENİDEN ENGELLEME ///////////////////////////////////////
+	//BEGENİDEN ENGELLEME 
 
 	$(document).on("mouseover", "html body:has(.groupJumpLayout:has(.groupsEditDescriptionArea ._4t38)) li.fbProfileBrowserListItem:not(.oki)", function() {
 		var grupid = $("input[name='profile_id']").attr("value");
@@ -37,10 +37,10 @@ $(function() {
 			$(this).find('div._42ef').parent().prepend("<div class='_6a rfloat _ohf'><div class='_6a _6b' style='height:50px'></div><div class='_6a _6b'><div class='_5t4x'><div><button class='_42ft _4jy0 FriendRequestAdd addButton _4jy3 _517h' type='button'><span class='tam_engel'><a href='/ajax/groups/members/remove.php?group_id=" + grupid + "&amp;uid=" + uyeid + "' rel='dialog-post' role='menuitem' tabindex='-1'><span><span>Block</span></span></a></span></button></div></div></div></div>");
 			$(this).addClass("oki");
 		}
-	}); //BEGENİDEN ENGELLEME ///////////////////////////////////////				
+	});
 
 
-	//MOUSEOVER SAKLA ///////////////////////////////////////
+	//MOUSEOVER SAKLA 
 	$(document).on("mouseover", "html body:has(#pagelet_pending_queue)", function() {
 		$('#pagelet_pending_queue .userContentWrapper').not(".kontrol").each(function(e) {
 			var idm = "deneme";
@@ -51,10 +51,10 @@ $(function() {
 			//	$(this).find("._51xa._528a:first").prepend('<a class="oto-onay" href="#" ajaxify="/ajax/groups/mall/preapprove?group_id='+grupid+'&amp;user_id='+kimo+'" rel="async-post" role="button">Auto</a>');
 			$(this).addClass("kontrol ");
 		});
-	}); //MOUSEOVER SAKLA ///////////////////////////////////////	 
+	});
 
-	//KONTROL ///////////////////////////////////////
-	$(document).on("mouseover", "html body:has(.groupJumpLayout:has(#pagelet_requests_queue))", function() {
+	//KONTROL 
+	/*$(document).on("mouseover", "html body:has(.groupJumpLayout:has(#pagelet_requests_queue))", function() {
 		$('#pagelet_requests_queue ul.uiList li._2rcr').not(".kontrol").each(function(e) {
 			var buhtml = $(this).text();
 			var zaman = $(this).find(".clearfix._8u._42ef ul.uiList.mts._2rct._4kg li:nth-child(1)").text();
@@ -84,5 +84,5 @@ $(function() {
 
 		});
 
-	});
+	});*/
 });
